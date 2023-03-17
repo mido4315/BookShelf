dbd7765cfe581391bdc87fb6c740959972c71d91
mido4315 <mohmed.mahmoud3@gmail.com>
2023-03-17 23:16:10 +0200

    Add project files.

---
+++ ebook store/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ebook_store.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ebook_store.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult MyHome()
+        {
+            return View();
+        }
+        public IActionResult BookDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
